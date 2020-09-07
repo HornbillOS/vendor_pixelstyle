462452ffc8b3b1c4ec99e68c8e0029864ae9d8f9
nahnah <nandanrmenon@gmail.com>
2020-09-07 12:00:15 +0530

    removed bootanimtion

--- config.mk
+++ config.mk
@@ -39,8 +39,6 @@ else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
      PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
      PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),2160)
-     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_2160.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
     ifeq ($(TARGET_BOOT_ANIMATION_RES),)
         $(warning "HornbillStyle: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
